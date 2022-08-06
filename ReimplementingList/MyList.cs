763b60e19763888ddeac7faa2a871ecc6789a6f1
GabrielWinter <gabrielwinter45@hotmail.com>
2022-08-06 19:42:06 -0300

    feat(): add List reimplementation.

---
+++ ReimplementingList/MyList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ReimplementingList
+{
+    class MyList<T> : IEnumerable<T>
+    {
+        public MyList() : this(16) { }
+        public MyList(int capacity)
+        {
+            this.Content = new T[capacity];
+            this.Count = 0;
+        }
+
+        public int Capacity => this.Content.Length;
+        public int Count { get; private set; }
+        private T[] Content { get; set; }
+
+        public T this[int index]
+        {
+            get => this.Content[this.ValidIndex(index)];
+            set => this.Content[this.ValidIndex(index)] = value;
+        }
+
+        private int ValidIndex(int index) =>
+            index < this.Count ? index : throw new IndexOutOfRangeException();
+
+        public void Add(T item)
+        {
+            this.EnsureCapacity(this.Count + 1);
+            this.Content[this.Count++] = item;
+        }
+
+        public void RemoveAt(int index)
+        {
+            Array.Copy(Content, ValidIndex(index) + 1, Content, index, Count - index - 1);
+            this.Count--;
+        }
+
+        public void EnsureCapacity(int capacity)
+        {
+            if (this.Capacity >= capacity) return;
+            int newCapacity = Math.Max(this.Capacity * 2, 1);
+            while (newCapacity < capacity) newCapacity *= 2;
+            T[] newContent = new T[newCapacity];
+            Array.Copy(this.Content, newContent, this.Count);
+            this.Content = newContent;
+        }
+
+        public void Clear() => this.Count = 0;
+
+        public void Sort() => Array.Sort(this.Content, 0, this.Count);
+
+        public void Sort(IComparer<T> comparer) => Array.Sort(this.Content, 0, this.Count, comparer);
+
+        public void ForEach(Action<T> action)
+        {
+            foreach (T item in this) action(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++) yield return this.Content[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        public Span<T> AsSpan() => new Span<T>(this.Content, 0, this.Count);
+    }
+}
---
+++ ReimplementingList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReimplementingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> data = Enumerable.Range(0, 20);
+
+            MyList<int> list = new();
+            foreach (int x in data) list.Add(x);
+            list.ForEach(x => Console.Write($"{x,3}"));
+            Console.WriteLine();
+
+            int[] array = new int[15];
+            int count = 0;
+
+            foreach (var x in data)
+            {
+                if (count == array.Length)
+                {
+                    int[] newArray = new int[array.Length * 2];
+                    Array.Copy(array, newArray, array.Length);
+                    array = newArray;
+                }
+
+                array[count++] = x;
+            }
+
+            for (int i = 0; i < count; i++) Console.Write($"{array[i],3}");
+            Console.WriteLine();
+        }
+    }
+}
